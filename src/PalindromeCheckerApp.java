2dedda229b8a5336ecf959894eca9f7584104d2c
Koushal1234 <koushedupulapati@gmail.com>
2026-02-27 15:46:18 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        String str = "madam";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Palindrome Checker App ===");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
         String reversed = "";
 
-        for(int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+        // Reverse string using for loop
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
-        if(str.equals(reversed)) {
-            System.out.println("It is a Palindrome");
+        // Compare original and reversed string
+        if (input.equals(reversed)) {
+            System.out.println("Result: It is a Palindrome");
         } else {
-            System.out.println("Not a Palindrome");
+            System.out.println("Result: Not a Palindrome");
         }
+
+        scanner.close();
     }
 }
