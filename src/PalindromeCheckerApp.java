d75f57b6f924773948c9ff7476714d6fe000989c
Koushal1234 <koushedupulapati@gmail.com>
2026-02-27 15:33:57 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,16 @@
-
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to palindromeChecker ManagementSystem");
-        System.out.println("Version :1.0");
-        System.out.println("System Initialized successfully.");
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = "";
+
+        for(int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if(str.equals(reversed)) {
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
     }
 }
