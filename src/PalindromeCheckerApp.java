7977f0fb7a32de0fd08c16fc2dc97b1c4ce58bf0
Koushal1234 <koushedupulapati@gmail.com>
2026-02-27 15:19:39 +0530

    UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,8 @@
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Hello PalindromeCheckerApp");
+        System.out.println("Welcome to palindromeChecker ManagementSystem");
+        System.out.println("Version :1.0");
+        System.out.println("System Initialized successfully.");
     }
 }
